ee1038519ea097c791044a036d14615480819624
Piotr Piszora <piotr.piszora@gmail.com>
2022-05-22 21:34:27 +0200

    posortowane jeszcze

--- main.py
+++ main.py
@@ -42,13 +42,11 @@ n = int(n)
 for i in range(n):
     x = eval(input())
     Egzemplarz(x[0],x[1],x[2])
+sorted(Ksiazka.ksiazki, key=lambda x: x[1], reverse=True)
 for i in range(len(Ksiazka.ksiazki)):
     print(Ksiazka(Ksiazka.ksiazki[i][0],Ksiazka.ksiazki[i][1],Ksiazka.ksiazki[i][2]))
 
 
-# egz2 = ("Pan Tadeusz", "Adam Slowacki", 2019)
-# print(egz2[0],egz2[1])
-# print(Ksiazka(egz2[0],egz2[1]))
 
 
         
